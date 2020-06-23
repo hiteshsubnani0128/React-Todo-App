1c367232d7a5c50892493eb6eb2d4644a8465483
Hitesh Subnani <hiteshsubnani734@gmail.com>
2020-06-23 20:48:41 +0530

    Updates

---
+++ precache-manifest.d0c1935945a174f515ff8bc8f40d8fac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa78e2b5d6f9bb78275e37fc7eaa91f9",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "40e3c9fc680ad9940474",
+    "url": "/react-todo-app/static/css/main.11fe36d2.chunk.css"
+  },
+  {
+    "revision": "13f33625c4ac2b8fbb4e",
+    "url": "/react-todo-app/static/js/2.27924908.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-todo-app/static/js/2.27924908.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40e3c9fc680ad9940474",
+    "url": "/react-todo-app/static/js/main.f756ad82.chunk.js"
+  },
+  {
+    "revision": "82291c363f51096875c4",
+    "url": "/react-todo-app/static/js/runtime-main.0aa18ffa.js"
+  }
+]);
